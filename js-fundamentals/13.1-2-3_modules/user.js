f28742d22ee8b3fa4a47f8d80d71bb0128bcce73
kushalbhalla7 <kushal.bhalla@appventurez.com>
2022-06-20 18:59:28 +0530

    Completed 13 Unit "Modules".

---
+++ js-fundamentals/13.1-2-3_modules/user.js
@@ -0,0 +1,62 @@
+// // export varaible 
+// export let num_set = [1, 2, 3, 4, 5];
+  
+// // export function
+// export default function hello() {
+//     console.log("Hello World!");
+// }
+
+// //export class
+// export class Greeting {
+//     constructor(name) {
+//         this.greeting = "Hello, " + name;
+//     }
+// }
+
+// export let admin = {
+//     name: "John"
+// };
+
+// // exporting a function that calculate date from dob
+// export let calculateAge = (dob) => {
+//     console.log();
+//     return new Date().getFullYear() - new Date(dob).getFullYear();
+// }
+
+
+// // // export multiple values
+
+// // export {admin, calculateAge};
+
+
+// export default class user {
+//     constructor() {
+//         this.name = "John"
+//     }
+
+//     fullName() {
+//         return `${this.name} Sayen`
+//     }
+// }
+
+
+// export function hi() {
+//     alert(`Hello`);
+//   }
+  
+//   export function bye() {
+//     alert(`Bye`);
+//   }
+  
+
+  export function hi() {
+    alert(`Hello`);
+  }
+  
+  export function bye() {
+    alert(`Bye`);
+  }
+  
+  export default function() {
+    alert("Module loaded (export default)!");
+  }
